affb0b11192b095d01937565ccd5c8f8b10fe354
lohitkumarvellore <96835018+lohitkumarvellore@users.noreply.github.com>
2022-02-02 14:53:11 +0530

    Delete inchestometers.java

--- inchestometers.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class inchestometers {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float inch = sc.nextFloat();
-        float meter = inch/39.37f;
-        System.out.println(meter + " meters");
-    }
-}
