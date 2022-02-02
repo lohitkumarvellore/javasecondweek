feb7e0354f3a391e6b1c11a1f1ee5ad2f655dbeb
lohitkumarvellore <96835018+lohitkumarvellore@users.noreply.github.com>
2022-02-02 14:52:55 +0530

    Delete converttemperature.java

--- converttemperature.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class converttemperature {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float fahrenheit = sc.nextFloat();
-        float celsius = ((fahrenheit-32)*5)/9;
-        System.out.println(celsius +" celsius");
-    }
-}
